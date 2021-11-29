2c316f8cef6bda50586cecb0f070c0ba3757a212
unknown <40063087+Dig-Dug@users.noreply.github.com>
2021-11-29 23:36:47 +0100

    -

--- d.js
+++ d.js
@@ -54,3 +54,203 @@
       );
     }
   }
+  ///////////ADD EVENT LISTENERS////////////////
+  class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: ''
+      };
+      this.handleEnter = this.handleEnter.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+ // Change code below this line
+ componentDidMount() {
+  document.addEventListener('keydown', this.handleKeyPress, true);
+    }
+    componentWillUnmount() {
+  document.removeEventListener('keydown', this.handleKeyPress, true);
+    }
+    // Change code above this line
+    handleEnter() {
+      this.setState((state) => ({
+        message: state.message + 'You pressed the enter key! '
+      }));
+    }
+    handleKeyPress(event) {
+      if (event.keyCode === 13) {
+        this.handleEnter();
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+      );
+    }
+  };
+
+
+  //Optimize Re-Renders with shouldComponentUpdate---------
+  class OnlyEvens extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    //shouldComponentUpdate()----props comparison
+    shouldComponentUpdate(nextProps, nextState) {
+
+      console.log('Should I update?');
+      // Change code below this line
+  if( nextProps.value % 2 == 0){ console.log("ee")
+  return  true;
+   } 
+    
+     // Change code above this line
+    }
+    componentDidUpdate() {
+      console.log('Component re-rendered.');
+    }
+    render() {
+      return <h1>{this.props.value}</h1>;
+    }
+  }
+  
+  class Controller extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 0
+      };
+      this.addValue = this.addValue.bind(this);
+    }
+    addValue() {
+      this.setState(state => ({
+        value: state.value + 1
+      }));
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.addValue}>Add</button>
+          <OnlyEvens value={this.state.value} />
+        </div>
+      );
+    }
+  }
+
+
+  ////Introducing Inline Styles-----------------
+ //check camelCase
+//<div style={{color: "yellow", fontSize: 16}}>Mellow Yellow</div>
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: "72px"}}>Big Red</div>
+    );
+  }
+};
+
+//Add Inline Styles in React-----------------
+const styles ={color:"purple",
+ fontSize: 40,
+ border:
+ "2px solid purple" }; 
+
+// Change code above this line
+class Colorful extends React.Component {
+  render() {
+    // Change code below this line
+    return (
+      <div style={{color: styles.color, fontSize: styles.fontSize,border: styles.border}}>Style Me!</div>
+    );
+    // Change code above this line
+  }
+};
+
+//Use Advanced JavaScript in React Render Method-----------------
+//You can also write JavaScript directly in your render methods,
+// before the return statement, without inserting it inside of curly braces
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class MagicEightBall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: '',
+      randomIndex: ''
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask() {
+    if (this.state.userInput) {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: ''
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
+  render() {
+    const possibleAnswers = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt',
+      'Yes, definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      "Don't count on it",
+      'My reply is no',
+      'My sources say no',
+      'Most likely',
+      'Outlook not so good',
+      'Very doubtful'
+    ];
+    let i = 0;
+    for(i in possibleAnswers){
+    const answer = Math.random(possibleAnswers[i])// Change this line
+    console.log(possibleAnswers[i]);
+  }; 
+    return (
+      <div>
+        <input
+          type='text'
+          value={this.state.userInput}
+          onChange={this.handleChange}
+          style={inputStyle}
+        />
+        <br />
+        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+        <br />
+        <h3>Answer:</h3>
+        <p>
+          {/* Change code below this line */}
+          {this.state.randomIndex}
+          {/* Change code above this line */}
+        </p>
+      </div>
+    );
+  }
+}
+
+
+
+
+
